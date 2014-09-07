35c7b1495e0e3ad776ef0bee23bf6afe9cb1ce4c
bumer7721 <bumer7721@gmail.com>
2014-09-07 16:31:18 +0300

    Initial commit

---
+++ src/main/java/com/autoclicker/App.java
@@ -0,0 +1,13 @@
+package com.autoclicker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
